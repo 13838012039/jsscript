60ba6c3bd8428a4b7dddb94d1ee8017aef31411e
13838012039 <40423506+13838012039@users.noreply.github.com>
2018-08-02 09:16:25 +0800

    Update demo.js

--- demo.js
+++ demo.js
@@ -6,6 +6,9 @@ var dirName = process.argv[2]
 fs.exists("./dirName",function(exists){
     if(exists)
     {
+        console.log("该文件已存在");
+    }
+    else{
         fs.mkdirSync("./" + dirName) // mkdir $1
         process.chdir("./" + dirName) // cd $1
         fs.mkdirSync('css') // mkdir css
@@ -16,9 +19,6 @@ fs.exists("./dirName",function(exists){
         fs.writeFileSync("./js/main.js", "var string = \"Hello World\"\nalert(string)")
         
         process.exit(0)
-    }
-    else{
-        console.log("该文件已存在");
     }
 })
 
