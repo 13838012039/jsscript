593eadbde2f454a67473e0fe1a1eacd2f4e949a7
Bunkang.Lee <liwenhansj@163.com>
2018-08-02 09:39:43 +0800

    tijiao

--- demo.js
+++ demo.js
@@ -1,24 +1,42 @@
 #!/usr/bin/env node
 
+// var fs = require('fs')
+// var dirName = process.argv[2]
+
+// fs.access("./dirName",function(err){
+//     if(err.code == "ENOENT"){
+//         fs.mkdirSync("./" + dirName) // mkdir $1
+//         process.chdir("./" + dirName) // cd $1
+//         fs.mkdirSync('css') // mkdir css
+//         fs.mkdirSync('js') // mkdir js
+        
+//         fs.writeFileSync("./index.html", "<!DOCTYPE>\n<title>Hello</title>\n<h1>Hi</h1>")
+//         fs.writeFileSync("css/style.css", "h1{color: red;}")
+//         fs.writeFileSync("./js/main.js", "var string = \"Hello World\"\nalert(string)")
+        
+//         process.exit(0)
+//     }
+//     else{
+//         console.log("sss");
+//     }
+// }) 不能用？？ exists也不能用？？
+
 var fs = require('fs')
 var dirName = process.argv[2]
-
-fs.exists("./dirName",function(exists){
-    if(exists)
-    {
-        console.log("该文件已存在");
-    }
-    else{
-        fs.mkdirSync("./" + dirName) // mkdir $1
-        process.chdir("./" + dirName) // cd $1
-        fs.mkdirSync('css') // mkdir css
-        fs.mkdirSync('js') // mkdir js
+try{
+fs.mkdirSync("./" + dirName) // mkdir $1
+process.chdir("./" + dirName) // cd $1
+fs.mkdirSync('css') // mkdir css
+fs.mkdirSync('js') // mkdir js
         
-        fs.writeFileSync("./index.html", "<!DOCTYPE>\n<title>Hello</title>\n<h1>Hi</h1>")
-        fs.writeFileSync("css/style.css", "h1{color: red;}")
-        fs.writeFileSync("./js/main.js", "var string = \"Hello World\"\nalert(string)")
+fs.writeFileSync("./index.html", "<!DOCTYPE>\n<title>Hello</title>\n<h1>Hi</h1>")
+fs.writeFileSync("css/style.css", "h1{color: red;}")
+fs.writeFileSync("./js/main.js", "var string = \"Hello World\"\nalert(string)")
         
-        process.exit(0)
-    }
-})
+process.exit(0)
+}
+catch(e){
+    console.log("已存在");
+}
+    //只能将错误拉出来说明已存在
 
